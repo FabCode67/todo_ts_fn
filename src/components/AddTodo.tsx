4279f7504e48eb42c9be33275479da066668f528
Fabrice <mwanafunzifabrice@gmail.com>
2023-06-08 23:13:08 +0200

    pushing

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,40 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-type Props = TodoProps & {
-    updateTodo: (todo: ITodo) => void
-    deleteTodo: (_id: string) => void
+type Props = { 
+  saveTodo: (e: React.FormEvent, formData: ITodo | any) => void 
 }
 
-const Todo: React.FC <Props> = ({
-    todo,
-    updateTodo,
-    deleteTodo
-}) => {
-    const checkTodo: string = todo.status ? `line-through` : ''
-    return (
-        <div className='Card'>
-            <div className='Card--text'>
-                <h1 className={checkTodo}>{todo.firstname}</h1>
-                <span className={checkTodo}>{todo.lastname}</span>
-                <span className={checkTodo}>{todo.email}</span>
-                <span className={checkTodo}>{todo.password}</span>
-                <span className={checkTodo}>{todo.age}</span>
-        </div>
-        <div className = 'Card--button'>
-            <button
-                onClick = {() => updateTodo(todo)}
-                className= {todo.status ? `hide-button` : 'Card--button__done'}
-                >
-                    Complete
-                </button>
-                <button
-                    onClick = {() => deleteTodo (todo._id)}
-                    className = 'Card--button__delete'
-                    >
-                        Delete
-                    </button>
-        </div>
-    </div>
-    )}
+const AddTodo: React.FC<Props> = ({ saveTodo }) => {
+  const [formData, setFormData] = useState<ITodo | {}>()
 
-    export default Todo
+  const handleForm = (e: React.FormEvent<HTMLInputElement>): void => {
+    setFormData({
+      ...formData,
+      [e.currentTarget.id]: e.currentTarget.value,
+    })
+  }
+
+  return (
+    <form className='Form' onSubmit={(e) => saveTodo(e, formData)}>
+      <div>
+        <div>
+          <label htmlFor='firstname'>firstname</label>
+          <input onChange={handleForm} type='text' id='firstname' />
+        </div>
+        <div>
+          <label htmlFor='lastename'>lastename</label>
+          <input onChange={handleForm} type='text' id='lastename' />
+        </div>
+        <div>
+          <label htmlFor='email'>email</label>
+          <input onChange={handleForm} type='email' id='email' />
+        </div>
+        <div>
+          <label htmlFor='passward'>passward</label>
+          <input onChange={handleForm} type='passward' id='passward' />
+        </div>
+        <div>
+          <label htmlFor='name'>age</label>
+          <input onChange={handleForm} type='number' id='age' />
+        </div>
+        <div>
+          <label htmlFor='status'>status</label>
+          <input onChange={handleForm} type='text' id='status' />
+        </div>
+    
+      </div>
+      <button disabled={formData === undefined ? true: false} >Add Todo</button>
+    </form>
+  )
+}
+
+export default AddTodo
